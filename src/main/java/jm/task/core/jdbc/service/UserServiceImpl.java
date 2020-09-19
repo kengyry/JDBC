87f079c88ef96c5a264347b4c7cda22aaad3fa90
kengy <kengyry.ken@yandex.ru>
2020-09-19 19:55:49 +0500

    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.ResultSet;
@@ -11,39 +13,18 @@ import jm.task.core.jdbc.util.Util;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-        try {
-            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
-            String sql = "CREATE TABLE USERS " +
-                    "(id BIGINT not NULL PRIMARY KEY AUTO_INCREMENT, " +
-                    " name VARCHAR(255) not NULL, " +
-                    " lastName VARCHAR(255) not NULL, " +
-                    " age TINYINT UNSIGNED not NULL)";
-            statement.executeUpdate(sql);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try {
-            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
-            String sql = "DROP TABLE IF EXISTS USERS";
-            statement.executeUpdate(sql);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
-            String sql = "INSERT INTO USERS VALUES (DEFAULT, '" + name + "', '" + lastName + "', " + age + ")";
-            statement.executeUpdate(sql);
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
@@ -51,28 +32,12 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<User> getAllUsers() {
-        List<User> list = new ArrayList<>();
-        try {
-            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
-            String sql = "SELECT id, name, lastName, age FROM USERS";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                list.add(new User(resultSet.getString(2),
-                        resultSet.getString(3), resultSet.getByte(4)));
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return list;
+        UserDao userDao = new UserDaoJDBCImpl();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try {
-            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
-            String sql = "TRUNCATE TABLE USERS";
-            statement.executeUpdate(sql);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.cleanUsersTable();
     }
 }
