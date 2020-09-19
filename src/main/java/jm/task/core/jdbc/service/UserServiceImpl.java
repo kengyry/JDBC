5600ab447319a7a0aba0a6c02470d6b040a95acf
kengy <kengyry.ken@yandex.ru>
2020-09-19 19:30:44 +0500

    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,18 +2,47 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import jm.task.core.jdbc.util.Util;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+        try {
+            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
+            String sql = "CREATE TABLE USERS " +
+                    "(id BIGINT not NULL PRIMARY KEY AUTO_INCREMENT, " +
+                    " name VARCHAR(255) not NULL, " +
+                    " lastName VARCHAR(255) not NULL, " +
+                    " age TINYINT UNSIGNED not NULL)";
+            statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
+            String sql = "DROP TABLE IF EXISTS USERS";
+            statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
+            String sql = "INSERT INTO USERS VALUES (DEFAULT, '" + name + "', '" + lastName + "', " + age + ")";
+            statement.executeUpdate(sql);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
@@ -22,10 +51,28 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        try {
+            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
+            String sql = "SELECT id, name, lastName, age FROM USERS";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                list.add(new User(resultSet.getString(2),
+                        resultSet.getString(3), resultSet.getByte(4)));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
-
+        try {
+            Statement statement = Util.ConnectionUtils.getConnection().createStatement();
+            String sql = "TRUNCATE TABLE USERS";
+            statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static class ConnectionUtils {
+
+        private static Connection connection;
+        private static final String URL = "jdbc:mysql://localhost:3306/mydb?useSSL=false&serverTimezone=UTC";
+        private static final String USER = "root";
+        private static final String PASS = "root";
+
+        public static Connection getConnection() {
+
+            try {
+                Driver driver = new com.mysql.cj.jdbc.Driver();
+                DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection(URL, USER, PASS);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            return connection;
+        }
+    }
 }
